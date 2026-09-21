f1704afa7f77687fb54ee7f268be278ec5594a82
valtorah <valtorah@uef.fi>
2026-09-21 14:33:18 +0200

    exercise 22

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Homepage from "./homepage.mdx";
+
 export default function Home() {
   return (
-    <main className="mx-auto w-full max-w-3xl p-6">
-      <h1 className="mb-2 text-2xl font-semibold">Blog list app</h1>
-      <p className="text-zinc-600 dark:text-zinc-400">
-        Welcome! Head over to the Blogs page to see the list of blogs.
-      </p>
+    <main className="markdown mx-auto w-full max-w-2xl p-6">
+      <Homepage />
     </main>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // include markdown and MDX files as page extensions
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
